565d1d38bc1d1799cc04c590af431361b1b552bf
medinisrao <95349846+medinisrao@users.noreply.github.com>
2022-01-11 14:04:39 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './Projects/weather-App/App';
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+ 
